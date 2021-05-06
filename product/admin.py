4b5d19b94c22febf1131b7bf6f3222591ccde3b8
Antropozen <enes.gokhan.ozcan@gmail.com>
2021-05-07 02:58:45 +0300

    Model Category ve Product ilişkisi

--- product/admin.py
+++ product/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category
+from product.models import Category, Product
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title','status']
     list_filter = ['status']
-
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','category','price','status']
+    list_filter = ['status']
 
 admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -19,3 +19,28 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Product(models.Model):
+    STATUS = (
+        ('TRUE','EVET'),
+        ('FALSE','HAYIR'),
+    )
+    category = models.ForeignKey(Category,on_delete= models.CASCADE) #relation with category table.
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    keywords = models.CharField(max_length=255)
+    image = models.ImageField(blank=True,upload_to='images/')
+    price = models.FloatField()
+    year = models.IntegerField()
+    fuel = models.CharField(max_length=50)
+    motor_power = models.IntegerField()
+    engine_capacity = models.IntegerField()
+    case_type = models.CharField(max_length=50)
+    detail = models.TextField()
+    status = models.CharField(max_length=10,choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
