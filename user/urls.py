e90cae5144dd29e1c2486888530e909d72d48f8a
Antropozen <enes.gokhan.ozcan@gmail.com>
2021-05-20 21:04:22 +0300

    Updating User Information and Password

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ex: /home/
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    #path('<int:question_id>/', views.detail, name='detail'),
-
+    path('update/', views.user_update, name='user_update'),
+    path('password/', views.user_password, name='user_password'),
 ]
